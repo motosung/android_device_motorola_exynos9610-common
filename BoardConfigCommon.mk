acbc9228cdd83c962a9d95ddf1b031951d17338d
Tim Zimmermann <tim@linux4.de>
2022-06-15 07:13:56 +0200

    exynos9610: PRODUCT_CFI_INCLUDE_PATHS is a product variable
    Change-Id: I6a6e259e3a6cf2aaef8820f40c3fa4bef8a3b931
    Signed-off-by: krazey <admin@krazey.de>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -160,4 +160,4 @@ BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_slsi
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
-PRODUCT_CFI_INCLUDE_PATHS += hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
+
