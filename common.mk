49677e081871d247be683d6d83b569dbec803d67
krazey <admin@krazey.de>
2023-04-15 18:59:24 +0200

    Revert "exynos9610: Use component-override to configure Bluetooth"
    This reverts commit 16d2cf8d4ebd2f527427a838cb9263bc9775618e.
    
    Signed-off-by: krazey <admin@krazey.de>

--- common.mk
+++ common.mk
@@ -364,10 +364,6 @@ PRODUCT_SOONG_NAMESPACES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# Sysconfig
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sysconfig/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
